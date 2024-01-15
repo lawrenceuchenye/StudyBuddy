856f5ca8bc2c1bc0931c1bfc68a328af81a42907
Adophilus <uchenna19of@gmail.com>
2024-01-15 00:38:02 +0000

    updated the channels repo

--- @studybuddy/backend/src/repositories/channel.ts
+++ @studybuddy/backend/src/repositories/channel.ts
@@ -1,38 +1,49 @@
-import { Types } from "mongoose"
+import { HydratedDocument, Types } from "mongoose"
 import { Channel, ChannelMessage, ChannelUser, IChannel, IChannelMessage, IChannelUser } from "@studybuddy/backend/models/channel"
-import { PaginationQueryOptions, createPaginatedResource } from "../utils/pagination"
+import Pagination from "../utils/pagination"
 import PermissionsManager from "../utils/permissions"
+import { Result } from "true-myth"
 
 export type CreateChannelPayload = Omit<IChannel, "createdAt">
 
 namespace ChannelRepository {
-  export async function createChannel(payload: CreateChannelPayload) {
-    const creator = await ChannelUser.create({
-      userId: payload.creatorId,
-      channelId: new Types.ObjectId(),
-      roles: ["CREATOR"],
-      joinedAt: new Date(),
-    })
+  export async function createChannel(payload: CreateChannelPayload): Promise<Result<HydratedDocument<IChannel>, string>> {
+    try {
+      const creator = await ChannelUser.create({
+        userId: payload.creatorId,
+        channelId: new Types.ObjectId(),
+        roles: ["CREATOR"],
+        joinedAt: new Date(),
+      })
 
-    const channel = await Channel.create({
-      ...payload,
-      creatorId: creator._id,
-      createdAt: new Date(),
-    })
+      const channel = await Channel.create({
+        ...payload,
+        creatorId: creator._id,
+        createdAt: new Date(),
+      })
 
-    creator.channelId = channel._id
-    await creator.save()
+      creator.channelId = channel._id
+      await creator.save()
 
-    return channel
+      return Result.ok(channel)
+    }
+    catch (err) {
+      return Result.err(err.message)
+    }
   }
 
   export type GetChannelPayload = {
     id: Types.ObjectId
   }
 
-  export async function getChannel(payload: GetChannelPayload) {
-    const channel = await Channel.findOne({ _id: payload.id })
-    return channel
+  export async function getChannel(payload: GetChannelPayload): Promise<Result<HydratedDocument<IChannel>, string>> {
+    try {
+      const channel = await Channel.findById({ _id: payload.id })
+      return Result.ok(channel)
+    }
+    catch (err) {
+      return Result.err(err.message)
+    }
   }
 
   export type ChannelQueryFilters = {
@@ -42,55 +53,77 @@ namespace ChannelRepository {
     createdAfter?: Date
   }
 
-  export async function getChannels(payload: GetMessagesInChannelPayload, filters: ChannelQueryFilters, paginationOptions: PaginationQueryOptions) {
-    const query = Channel
-      .find({
-        _id: payload.channelId,
-        name: new RegExp(filters.name, "i"),
-        subjects: {
-          $all: filters.subjects ?? []
-        },
-        createdAt: {
-          $lte: filters.createdBefore,
-          $gte: filters.createdAfter
-        },
-      })
-      .skip(paginationOptions.perPage * paginationOptions.page - 1)
-      .limit(paginationOptions.perPage)
+  export async function getChannels(payload: GetMessagesInChannelPayload, filters: ChannelQueryFilters, paginationOptions: Pagination.QueryOptions): Promise<Result<Pagination.PaginatedResource<HydratedDocument<IChannel>>, string>> {
+    try {
+      const query = Channel
+        .find({
+          _id: payload.channelId,
+          name: new RegExp(filters.name, "i"),
+          subjects: {
+            $all: filters.subjects ?? []
+          },
+          createdAt: {
+            $lte: filters.createdBefore,
+            $gte: filters.createdAfter
+          },
+        })
+        .skip(paginationOptions.perPage * paginationOptions.page - 1)
+        .limit(paginationOptions.perPage)
 
-    const channelMessages = await query
-      .exec()
+      const channelMessages = await query
+        .exec()
 
-    const total = await query.countDocuments()
-    return createPaginatedResource(channelMessages, { ...paginationOptions, total })
+      const total = await query.countDocuments()
+      return Result.ok(Pagination.createPaginatedResource(channelMessages, { ...paginationOptions, total }))
+    }
+    catch (err) {
+      return Result.err(err.message)
+    }
   }
+}
 
-  export type UpdateChannelPayload = Omit<IChannel, "creatorId" | "createdAt"> & {
-    id: Types.ObjectId
-  }
+export type UpdateChannelPayload = Omit<IChannel, "creatorId" | "createdAt"> & {
+  id: Types.ObjectId
+}
 
-  export async function updateChannel(payload: UpdateChannelPayload) {
+export async function updateChannel(payload: UpdateChannelPayload): Promise<Result<undefined, string>> {
+  try {
     const { id, ...updatePayload } = payload
     const { acknowledged } = await Channel.updateOne({ _id: id }, updatePayload)
-    return acknowledged
-  }
 
-  export type DeleteChannelPayload = {
-    id: Types.ObjectId
+    if (acknowledged) return Result.ok(undefined)
+    return Result.err(undefined)
   }
+  catch (err) {
+    return Result.err(err.message)
+  }
+}
 
-  export async function deleteChannel(payload: DeleteChannelPayload) {
+export type DeleteChannelPayload = {
+  id: Types.ObjectId
+}
+
+export async function deleteChannel(payload: DeleteChannelPayload): Promise<Result<undefined, string>> {
+  try {
     const { acknowledged } = await Channel.deleteOne({ _id: payload.id })
-    return acknowledged
+    await ChannelUser.deleteMany({ channelId: payload.id })
+
+    if (acknowledged) return Result.ok(undefined)
+    return Result.err(undefined)
   }
+  catch (err) {
+    return Result.err(err.message)
+  }
+}
 
-  export type AddMessageToChannelPayload = Omit<IChannelMessage, "sentAt">
+export type AddMessageToChannelPayload = Omit<IChannelMessage, "sentAt">
 
-  export async function addMessageToChannel(payload: AddMessageToChannelPayload) {
+export async function addMessageToChannel(payload: AddMessageToChannelPayload): Promise<Result<HydratedDocument<IChannelMessage>, string>> {
+  try {
     const sender = await ChannelUser.findOne({ _id: payload.senderId, channelId: payload.channelId })
 
     if (!PermissionsManager.ChannelUser(sender).can("post", "ChannelMessage")) {
-      return false
+      return Result.err("User doesn't have permission to send messages")
     }
 
     const message = await ChannelMessage.create({
@@ -98,20 +131,25 @@ namespace ChannelRepository {
       sentAt: new Date(),
     })
 
-    return message
+    return Result.ok(message)
   }
-
-  export type GetMessagesInChannelPayload = {
-    channelId: Types.ObjectId
+  catch (err) {
+    return Result.err(err.message)
   }
+}
 
-  export type ChannelMessageQueryFilters = {
-    contains?: string
-    before?: Date
-    after?: Date
-  }
+export type GetMessagesInChannelPayload = {
+  channelId: Types.ObjectId
+}
 
-  export async function getMessagesInChannel(payload: GetMessagesInChannelPayload, filters: ChannelMessageQueryFilters, paginationOptions: PaginationQueryOptions) {
+export type ChannelMessageQueryFilters = {
+  contains?: string
+  before?: Date
+  after?: Date
+}
+
+export async function getMessagesInChannel(payload: GetMessagesInChannelPayload, filters: ChannelMessageQueryFilters, paginationOptions: Pagination.QueryOptions): Promise<Result<Pagination.PaginatedResource<HydratedDocument<IChannelMessage>>, string>> {
+  try {
     const query = ChannelMessage
       .find({
         _id: payload.channelId,
@@ -128,62 +166,92 @@ namespace ChannelRepository {
       .exec()
 
     const total = await query.countDocuments()
-    return createPaginatedResource(channelMessages, { ...paginationOptions, total })
+    return Result.ok(Pagination.createPaginatedResource(channelMessages, { ...paginationOptions, total }))
   }
-
-  export type UpdateMessageInChannelPayload = Omit<IChannelMessage, "sentAt" | "channelId" | "senderId"> & {
-    id: Types.ObjectId
-    channelId: Types.ObjectId
+  catch (err) {
+    return Result.err(err.message)
   }
+}
 
-  export async function updateMessageInChannel(payload: UpdateMessageInChannelPayload) {
+export type UpdateMessageInChannelPayload = Omit<IChannelMessage, "sentAt" | "channelId" | "senderId"> & {
+  id: Types.ObjectId
+  channelId: Types.ObjectId
+}
+
+export async function updateMessageInChannel(payload: UpdateMessageInChannelPayload): Promise<Result<undefined, string>> {
+  try {
     const { id, channelId, ...updatePayload } = payload
     const { acknowledged } = await ChannelMessage.updateOne({ _id: id, channelId }, updatePayload)
-    return acknowledged
+
+    if (acknowledged) return Result.ok(undefined)
+    return Result.err(undefined)
   }
-
-  export type DeleteMessageInChannelPayload = {
-    id: Types.ObjectId
-    channelId: Types.ObjectId
+  catch (err) {
+    return Result.err(err.message)
   }
+}
 
-  export async function deleteMessageInChannel(payload: DeleteMessageInChannelPayload) {
-    const { id, channelId } = payload
-    const { acknowledged } = await ChannelMessage.updateOne({ _id: id, channelId }, { deleted: true, content: "", mediaIds: [] })
-    return acknowledged
-  }
+export type DeleteMessageInChannelPayload = {
+  id: Types.ObjectId
+  channelId: Types.ObjectId
+}
 
-  export type AddUserToChannelPayload = Omit<IChannelUser, "roles" | "joinedAt">
+export async function deleteMessageInChannel(payload: DeleteMessageInChannelPayload) {
+  const { id, channelId } = payload
+  const { acknowledged } = await ChannelMessage.updateOne({ _id: id, channelId }, { deleted: true, content: "", mediaIds: [] })
+  return acknowledged
+}
 
-  export async function addUserToChannel(payload: AddUserToChannelPayload) {
+export type AddUserToChannelPayload = Omit<IChannelUser, "roles" | "joinedAt">
+
+export async function addUserToChannel(payload: AddUserToChannelPayload): Promise<Result<HydratedDocument<IChannelUser>, string>> {
+  try {
     const user = await ChannelUser.create({
       ...payload,
       roles: ["MEMBER"],
       joinedAt: new Date()
     })
-    return user
+    return Result.ok(user)
   }
+  catch (err) {
+    return Result.err(err.message)
+  }
+}
 
-  export type UpdateUserInChannelPayload = Omit<IChannelUser, "joinedAt">
+export type UpdateUserInChannelPayload = Omit<IChannelUser, "joinedAt">
 
-  export async function updateUserInChannel(payload: UpdateUserInChannelPayload) {
+export async function updateUserInChannel(payload: UpdateUserInChannelPayload): Promise<Result<undefined, string>> {
+  try {
     const { userId, channelId, ...updatePayload } = payload
     const roles = new Set(updatePayload.roles)
     roles.delete("CREATOR")
     roles.delete("MEMBER")
     roles.add("MEMBER")
     const { acknowledged } = await ChannelUser.updateOne({ _id: userId, channelId }, { roles })
-    return acknowledged
-  }
 
-  export type RemoveUserFromChannelPayload = {
-    userId: Types.ObjectId
-    channelId: Types.ObjectId
+    if (acknowledged) return Result.ok(undefined)
+    return Result.err(undefined)
   }
+  catch (err) {
+    return Result.err(err.message)
+  }
+}
 
-  export async function removeUserFromChannel(payload: RemoveUserFromChannelPayload) {
+export type RemoveUserFromChannelPayload = {
+  userId: Types.ObjectId
+  channelId: Types.ObjectId
+}
+
+export async function removeUserFromChannel(payload: RemoveUserFromChannelPayload): Promise<Result<undefined, string>> {
+  try {
     const { userId, channelId } = payload
-    await ChannelUser.deleteOne({ _id: userId, channelId })
+    const { acknowledged } = await ChannelUser.deleteOne({ _id: userId, channelId })
+
+    if (acknowledged) return Result.ok(undefined)
+    return Result.err(undefined)
+  }
+  catch (err) {
+    return Result.err(err.message)
   }
 }
 
