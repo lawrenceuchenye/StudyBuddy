7d0933db8d64b2be4a7258f340535ce3b2ca2988
Adophilus <uchenna19of@gmail.com>
2024-01-15 01:14:54 +0000

    updated the permissions manager

--- @studybuddy/backend/src/utils/permissions.ts
+++ @studybuddy/backend/src/utils/permissions.ts
@@ -1,11 +1,15 @@
 import { defineAbility } from '@casl/ability';
 import { IChannelUser } from '../models/channel';
+import { HydratedDocument } from 'mongoose';
 
 namespace PermissionsManager {
-  export const ChannelUser = (user: IChannelUser) => defineAbility((can) => {
+  export const ChannelUser = (user: HydratedDocument<IChannelUser>) => defineAbility((can) => {
     if (user.roles.includes("POSTER") || user.roles.includes("MODERATOR") || user.roles.includes("CREATOR")) {
       can('post', 'ChannelMessage')
     }
+
+    can('delete', 'Channel', { creatorId: user._id })
   })
 }
+
 export default PermissionsManager
