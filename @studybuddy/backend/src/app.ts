400c05909adc38006163e56f187cf305b9151dc2
Adophilus <uchenna19of@gmail.com>
2024-01-15 01:15:24 +0000

    added some code to connect to the db

--- @studybuddy/backend/src/app.ts
+++ @studybuddy/backend/src/app.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { Router as channelsRouter } from './routes/channel'
+import Database from './utils/database'
+
+Database.start()
 
 export const app = new Hono()
   .use("*", logger())
