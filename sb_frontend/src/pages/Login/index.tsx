814abb15f68eb58b15eec1691418426a13d49c7d
lawrenceuchenye <lawuche29@gmail.com>
2024-01-29 01:28:08 +0100

    links done

--- sb_frontend/src/pages/Login/index.tsx
+++ sb_frontend/src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import { FC, useEffect } from "react";
 import { useStudyBudStore } from "../../store/";
+import { NavLink } from "react-router-dom";
 
 const index: FC = () => {
   const setIsTapped = useStudyBudStore((state) => state.setIsTapped);
@@ -22,6 +23,16 @@ const index: FC = () => {
           <div className="form-fields-container">
             <input type="text" placeholder="@email address" />
             <input type="password" placeholder="password" />
+
+            <div className="login-txt">
+              <p>
+                Don't have an account,
+                <NavLink to="/login" style={{ color: "var(--color-blue)" }}>
+                  <a>Sign Up</a>
+                </NavLink>
+              </p>
+            </div>
+
             <button>Log in</button>
           </div>
         </div>
