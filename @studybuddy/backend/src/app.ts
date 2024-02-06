24f818a088b87beea194dd39a9a0d32e9f0dcaa0
Adophilus <uchenna19of@gmail.com>
2024-02-06 15:50:04 +0100

    refactored the app a bit

--- @studybuddy/backend/src/app.ts
+++ @studybuddy/backend/src/app.ts
@@ -3,20 +3,10 @@ import { logger } from "hono/logger";
 import { swaggerUI } from '@hono/swagger-ui'
 import { serveStatic } from '@hono/node-server/serve-static'
 
-import { Router as channelsRouter } from "./routes/channel";
-import { Router as authRouter } from "./routes/auth";
-import { Router as mediaRouter } from "./routes/media";
-import { Router as studyGroupRouter } from "./routes/study-group";
-import { Router as resourcesRouter } from "./routes/resource";
-
 import Database from "./utils/database";
-import { fromZodError } from "zod-validation-error";
-import { ZodError } from "zod";
-import { StatusCodes } from "http-status-codes";
-import GlobalLogger from "./utils/logger";
-import { APIError } from "./utils/error";
+import routes from "./routes";
+import { handler as errorHandler } from "./utils/error"
 
-const errorLogger = GlobalLogger.getSubLogger({ name: "ErrorLogger" });
 const stopDatabase = await Database.start();
 
 const gracefulShutdown = async () => {
@@ -33,28 +23,7 @@ export const app = new Hono()
 	.use("*", logger())
 	.get('/static/*', serveStatic({ root: './' }))
 	.get('/ui', swaggerUI({ url: '/static/openapi.yaml' }))
-	.route("/auth", authRouter)
-	.route("/media", mediaRouter)
-	.route("/channels", channelsRouter)
-	.route("/study-groups", studyGroupRouter)
-	.route("/resources", resourcesRouter)
-	.onError((err, c) => {
-		if (err instanceof ZodError) {
-			return c.json({ error: fromZodError(err) }, 400);
-		}
-
-		if (err instanceof APIError) {
-			return c.json({ error: err.message }, err.code)
-		}
-
-		errorLogger.error(err);
-
-		return c.json(
-			{
-				message: "Internal server error",
-			},
-			StatusCodes.INTERNAL_SERVER_ERROR
-		);
-	});
+	.route("/", routes)
+	.onError(errorHandler);
 
 export type App = typeof app;
