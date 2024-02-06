b9f5ddb7578e372644db7abb101c06c68ccc2c20
lawrenceuchenye <lawuche29@gmail.com>
2024-02-06 20:13:02 +0100

    we still on it.

---
+++ sb_frontend/src/pages/About/index.tsx
@@ -0,0 +1,20 @@
+import "./index.css";
+import React from "react";
+
+const index = (props: {}) => {
+  return (
+    <div className="main-about-container">
+      <h1>About Us</h1>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse officia
+          eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit
+        </p>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
