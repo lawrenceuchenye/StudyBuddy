8c41c0172b60c95ef22239bcd088734e8b2b218f
lawrenceuchenye <lawuche29@gmail.com>
2024-02-06 23:45:22 +0100

    finally got progress on text editor

--- sb_frontend/src/pages/DashboardTutor/index.tsx
+++ sb_frontend/src/pages/DashboardTutor/index.tsx
@@ -1,12 +1,17 @@
 import "./index.css";
 import DashboardNavbar from "../../components/DashboardNavbar/";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useStudyBudStore } from "../../store/";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const index: FC = () => {
   const isDashboardNavActive = useStudyBudStore(
     (state) => state.isDashboardNavActive,
   );
+  const [value, setValue] = useState('');
+
+
 
   return (
     <div className="dashboard-main-container">
@@ -30,6 +35,16 @@ const index: FC = () => {
               <h4>77 Massachusetts Ave</h4>
               <p>3rd year - Mech. Eng.</p>
             </div>
+          </div>
+        </div>
+        <div>
+
+          <div>
+            <input type="text" placeholder="title" />
+            <ReactQuill theme="snow" value={value} onChange={setValue} />
+          </div>
+          <div>
+            <p>{value}</p>
           </div>
         </div>
       </div>
