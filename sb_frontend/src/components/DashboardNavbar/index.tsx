800b2bae930d1f13d33f0d03d66dd17140015a51
lawrenceuchenye <lawuche29@gmail.com>
2024-02-09 18:28:10 +0100

    designing dashboard mobile nav

--- sb_frontend/src/components/DashboardNavbar/index.tsx
+++ sb_frontend/src/components/DashboardNavbar/index.tsx
@@ -5,6 +5,8 @@ import { NavLink } from "react-router-dom";
 
 const index: FC = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [width, setWidth] = useState<number | null>(null);
+
 
   const isDashboardNavActive = useStudyBudStore(
     (state) => state.isDashboardNavActive,
@@ -14,50 +16,51 @@ const index: FC = () => {
   );
 
   useEffect(() => {
+    setWidth(window.innerWidth);
     if (window.innerWidth < 750) {
       setIsMobile(true);
       return;
     }
 
     setIsMobile(false);
-  }, []);
+  }, [window.innerWidth]);
 
   if (isMobile) {
     return (
-      <div className="mb-nav-container">
+      <div className="mb-nav-container" style={{ width: `${width}px` }}>
         <NavLink
           to="/dashboard"
           style={{ color: "var(--color-white)", textDecoration: "none" }}
         >
-          <div className="link" onClick={(e) => e.stopPropagation()}>
+          <div className="mb-link" onClick={(e) => e.stopPropagation()}>
             <i className="fa-solid fa-user"></i>
 
             <h1>Profile</h1>
           </div>
         </NavLink>
 
-        <div className="link" onClick={(e) => e.stopPropagation()}>
+        <div className="mb-link" onClick={(e) => e.stopPropagation()}>
           <i className="fa-solid fa-bullseye"></i>
 
           <h1>Goals</h1>
         </div>
-        <div className="link" onClick={(e) => e.stopPropagation()}>
+        <div className="mb-link" onClick={(e) => e.stopPropagation()}>
           <i className="fa-solid fa-users"></i>
 
           <h1>Match</h1>
         </div>
-        <div className="link" onClick={(e) => e.stopPropagation()}>
+        <div className="mb-link" onClick={(e) => e.stopPropagation()}>
           <i className="fa-solid fa-chalkboard-teacher"></i>
 
           <h1>Tutor</h1>
         </div>
-        <div className="alink">
-          <div className="link" onClick={(e) => e.stopPropagation()}>
+        {/*<div className="alink">
+          <div className="mb-link" onClick={(e) => e.stopPropagation()}>
             <i className="fa-solid fa-share"></i>
 
             <h1>Share</h1>
           </div>
-        </div>
+        </div>*/}
       </div>
     );
   }
